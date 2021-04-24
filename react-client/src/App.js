11a10fea7b3ee7b7e87ea496f53192f5197b8bd2
ch2isk4kos <ch2isk4kos@gmail.com>
2021-04-23 23:29:11 -0400

    import Greeter contract, ethers library and set contract address to variable

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,9 +1,16 @@
+import React, { useState } from "react";
+import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json";
+import { ethers } from "ethers";
 import "./styles/App.css";
+
+// store contract address in a varibale where Greeter deployed to:
+const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 const App = () => {
   return (
     <div className="App">
       <h2>React + Ethereum: Decentralized Application</h2>
+      <form onSubmit={handleOnSubmit}></form>
     </div>
   );
 };
