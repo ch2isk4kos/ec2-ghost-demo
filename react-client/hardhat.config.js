82af69a90e789c495a0fd54747ece87e01632d3d
ch2isk4kos <ch2isk4kos@gmail.com>
2021-04-23 20:25:00 -0400

    add configurations for compilation paths and additional networks

--- react-client/hardhat.config.js
+++ react-client/hardhat.config.js
@@ -16,7 +16,17 @@ task("accounts", "Prints the list of accounts", async () => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  solidity: "0.7.3",
-};
 
+// Where the project configuration lives:
+module.exports = {
+  // solidity: "0.7.3",
+  solidity: "0.8.3",
+  paths: {
+    artifacts: "./src/artifacts",
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
+};
