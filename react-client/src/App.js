03fc0dfba33175b93e2429c1460def7f830af94c
ch2isk4kos <ch2isk4kos@gmail.com>
2021-04-24 17:14:20 -0400

    add input field and button and attach functionality to user interface

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,14 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json";
 import { ethers } from "ethers";
 import "./styles/App.css";
 
 // store contract address in a varibale where Greeter deployed to:
-const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const greeterAddress = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9";
 
 const App = () => {
   const [greeting, setGreeting] = useState("");
-  const [data, setData] = useState("");
+  // const [account, setAccount] = useState("");
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    fetchGreeting();
+    // fetchGreetings();
+  }, []);
+
+  // const fetchGreetings = async () => {
+  //   if (typeof window.ethereum !== "undefined") {
+  //     const provider = new ethers.providers.JsonRpcProvider();
+  //     await provider.getBlockNumber().then((res) => {
+  //       console.log("response:", res.data);
+  //     });
+  //   }
+  // };
 
   const fetchGreeting = async () => {
     if (typeof window.ethereum !== "undefined") {
@@ -23,45 +38,61 @@ const App = () => {
 
       try {
         const d = await contract.greet();
-        setData(d);
-        console.log("data: ", d);
+        console.log("greeting content: ", d);
+        setContent(d);
       } catch (err) {
         console.log("error: ", err);
       }
     }
   };
 
-  const handleOnAccountRequest = async () => {
+  const requestAccount = async () => {
     await window.ethereum.request({ method: "eth_requestAccounts" }); // prompts user to connect MetaMask account
   };
 
-  const handleOnGreetingSignature = async () => {
+  // const handleOnGreetingInput = (e) => {
+  //   e.preventDefault();
+  //   console.log(e.target.value);
+  //   setGreeting(e.target.value);
+  // };
+
+  const handleOnGreetingUpdate = async () => {
     if (!greeting) return;
 
     if (typeof window.ethereum !== "undefined") {
-      await handleOnAccountRequest();
+      await requestAccount();
 
       const provider = new ethers.providers.Web3Provider(window.ethereum);
+      console.log({ provider });
       const signer = provider.getSigner();
       const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
       const transaction = await contract.setGreeting(greeting); // set user input
+      await transaction.wait().then((res) => {
+        console.log(res);
+        setGreeting("");
+      }); // waiting for blockchain confirmation
 
-      await transaction.wait(); // waiting for blockchain confirmation
-
+      console.log({ transaction });
       fetchGreeting();
     }
   };
 
-  const handleOnSubmit = () => {
-    //
-    //
-    //
-  };
-
   return (
     <div className="App">
-      <h2>React + Ethereum: Decentralized Application</h2>
-      <form></form>
+      <header className="App-header">
+        <h2>React + Ethereum: Decentralized Application</h2>
+        {!content ? <h2>No Greeting Found</h2> : <h2>{content}</h2>}
+        {/* <button onClick={fetchGreeting}>GET</button> */}
+        {/* <form onSubmit={handleOnGreetingUpdate}> */}
+        <input
+          value={greeting}
+          onChange={(e) => setGreeting(e.target.value)}
+          placeholder="Set Greeting"
+          required
+        />
+        <button onClick={handleOnGreetingUpdate}>SIGN</button>
+        {/* </form> */}
+      </header>
     </div>
   );
 };
